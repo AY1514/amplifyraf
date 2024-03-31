053d79a40a08eb0d2159c360cc9fa82488e26632
AY1514 <AY1514@users.noreply.github.com>
2024-03-30 22:20:06 -0400

    galatians 2:20

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Galatians 2:20</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        I have been tcrucified with Christ. 
+        It is no longer I who live, but Christ who lives uin me. 
+        And the life I now live in the flesh I live by faith in the Son of God,
+        vwho loved me and wgave himself for me.
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
